d3fe383176d9f9c77aca7417424683d442432a3e
Natalia <nataliya.krotava@innowise-group.com>
2021-10-05 18:01:08 +0300

    add: firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/compat';
+import { auth } from './firebase/firebase';
+
+const Context = createContext<any>(null);
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Context.Provider
+            value={{
+                firebase,
+                auth,
+            }}
+        >
+            <App />
+        </Context.Provider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
