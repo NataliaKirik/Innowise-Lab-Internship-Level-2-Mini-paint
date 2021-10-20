af179390b4233a0db4d1e44b33337f79ab157be2
Natalia <nataliya.krotava@innowise-group.com>
2021-10-20 15:46:12 +0300

    refactor: delete useEffect

--- src/pages/paint/Paint.tsx
+++ src/pages/paint/Paint.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import { AppRootStateType } from '../../app/store';
 import { Redirect } from 'react-router-dom';
 import { PATH } from '../../common/constants/routes';
@@ -7,12 +7,7 @@ import Toolbar from '../../common/components/toolbar/Toolbar';
 import Canvas from '../../common/components/canvas/Canvas';
 
 export const Paint = () => {
-    const dispatch = useDispatch();
     const isAuth = useSelector<AppRootStateType, boolean>((state) => state.login.isAuth);
-
-    useEffect(() => {
-        if (isAuth) return;
-    }, [isAuth, dispatch]);
 
     if (!isAuth) return <Redirect to={PATH.LOGIN} />;
 
